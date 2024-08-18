4fcb178b3099579f19ec826f24dc984b47ff66dc
hm904049@gmail.com <you@example.com>
2024-08-19 01:50:49 +0400

    File Add

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup
+from typing import List 
+
+def get_requirements()
+
+
+
+
+setup(
+    Name='Again',
+    version='0.0.2',
+    author='Hammad Asif ',
+    author_email='hm904049@gmail.com',
+    packages=find_packages(),
+    install_requir=get_requirements('requirements.txt'),
+)
